c8476286d6729792893b17aeb24ebe7482d93610
Tomasz Molis <molitom@chrobinson.com>
2024-09-03 13:01:31 +0200

    extra components

--- src/crm/crm.module.ts
+++ src/crm/crm.module.ts
@@ -1,10 +1,14 @@
 import { Module } from '@nestjs/common';
 import { SectionsController } from './controllers/sections.controller';
 import { SectionsService } from './services/sections.service';
+import { ArticlesController } from './controllers/articles.controller';
+import { ImagesController } from './controllers/images.controller';
+import { ImagesService } from './services/images.service';
+import { ArticlesService } from './services/articles.service';
 
 
 @Module({
-  controllers: [SectionsController],
-  providers: [SectionsService],
+  controllers: [SectionsController, ArticlesController, ImagesController],
+  providers: [SectionsService, ImagesService, ArticlesService],
 })
 export class CrmModule {}
